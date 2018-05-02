305a764ea49f95660c4c61d51786fee426c6e08c
Pablo Galante <pablo.galante5143@est.uca.edu.ni>
2018-05-02 12:18:52 -0600

    Modelo token

--- app/src/main/java/com/momotombodevs/pgalante/bmi_calculator/Api/ApiInterface.java
+++ app/src/main/java/com/momotombodevs/pgalante/bmi_calculator/Api/ApiInterface.java
@@ -1,6 +1,9 @@
 package com.momotombodevs.pgalante.bmi_calculator.Api;
 
+import android.media.session.MediaSession;
+
 import com.momotombodevs.pgalante.bmi_calculator.models.LoginModel;
+import com.momotombodevs.pgalante.bmi_calculator.models.TokenModel;
 import com.momotombodevs.pgalante.bmi_calculator.models.UserModel;
 
 import java.util.ArrayList;
@@ -9,12 +12,13 @@ import retrofit2.Call;
 import retrofit2.http.Body;
 import retrofit2.http.DELETE;
 import retrofit2.http.GET;
+import retrofit2.http.Header;
 import retrofit2.http.POST;
 import retrofit2.http.PUT;
 import retrofit2.http.Path;
 
 public interface ApiInterface {
-    @POST("/users/login")
+    @POST("users/login")
     Call<UserModel> login(@Body LoginModel login);
 
     @POST("users")
@@ -26,4 +30,6 @@ public interface ApiInterface {
     @DELETE("users/{id}")
     Call<UserModel> deleteUser(@Path("id") String id);
 
+    @GET("users/login")
+    Call<TokenModel> getToken(@Header("id")String id);
 }
---
+++ app/src/main/java/com/momotombodevs/pgalante/bmi_calculator/activities/GetToken.java
@@ -0,0 +1,5 @@
+package com.momotombodevs.pgalante.bmi_calculator.activities;
+
+public class GetToken {
+
+}
