c1fc447b29856bb763398233d52515ed70f23c59
PabloIsMurdoc <pablo.galante5143@est.uca.edu.ni>
2018-05-03 21:31:22 -0600

    Layout resultados

---
+++ app/src/main/java/com/momotombodevs/pgalante/bmi_calculator/activities/Result.java
@@ -0,0 +1,15 @@
+package com.momotombodevs.pgalante.bmi_calculator.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.momotombodevs.pgalante.bmi_calculator.R;
+
+public class Result extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_result);
+    }
+}
